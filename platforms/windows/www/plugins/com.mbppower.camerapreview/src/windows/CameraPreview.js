ad7f1ac8e2bacc383f537c9b5608f215f0bfd97b
lpmusicon <lpmusicon@gmail.com>
2016-04-27 00:53:29 +0200

    CameraPreview on Windows is working

--- platforms/windows/www/plugins/com.mbppower.camerapreview/src/windows/CameraPreview.js
+++ platforms/windows/www/plugins/com.mbppower.camerapreview/src/windows/CameraPreview.js
@@ -12,52 +12,36 @@ module.exports = {
         }
     },
     startCamera: function (successCallback, errorCallback, setup) {
-        var Capture = Windows.Media.Capture;
-        var DeviceInformation = Windows.Devices.Enumeration.DeviceInformation;
-        var DeviceClass = Windows.Devices.Enumeration.DeviceClass;
-        var DisplayOrientations = Windows.Graphics.Display.DisplayOrientations;
-        var FileProperties = Windows.Storage.FileProperties;
-        var Media = Windows.Media;
-        var SimpleOrientation = Windows.Devices.Sensors.SimpleOrientation;
-        var SimpleOrientationSensor = Windows.Devices.Sensors.SimpleOrientationSensor;
+        var mediaRec = new Windows.Media.Capture.MediaCapture();
+        var div = document.getElementById("windowsPreview");
+        var settings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
 
-        var oOrientationSensor = SimpleOrientationSensor.getDefault();
-        var oDisplayInformation = Windows.Graphics.Display.DisplayInformation.getForCurrentView();
-        var oDeviceOrientation = SimpleOrientation.notRotated;
-        var oDisplayOrientation = DisplayOrientations.portrait;
+        
 
-        var oDisplayRequest = new Windows.System.Display.DisplayRequest();
-        var oSystemMediaControls = Media.SystemMediaTransportControls.getForCurrentView();
+        var deviceList = [];
+        function enumerateCameras() {
+            var deviceInfo = Windows.Devices.Enumeration.DeviceInformation;
+            deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture).then(function (devices) {
+                // Add the devices to deviceList                                                                                                               
+                if (devices.length > 0) {
 
-        var oMediaCapture = null;
-        var isInitialized = false;
-        var isPreviewing = false;
-        var isRecording = false;
+                    for (var i = 0; i < devices.length; i++) {
+                        deviceList.push(devices[i]);
+                    }
+                } else {
+                    console.log("No devices found");
+                }
+            }, function () { console.log("error"); });
+        }
 
-        var externalCamera = false;
-        var mirroringPreview = false;
+        enumerateCameras();
+        settings.videoDeviceId = deviceList[0];
 
-        var RotationKey = "C380465D-2271-428C-9B83-ECEA3B4A85C1";
-        var deviceInfo = null;
-        DeviceInformation.findAllAsync(DeviceClass.videoCapture).then(function (devices) {
-            if (devices.length > 0) {
-                deviceInfo = devices.getAt(0);
-            }
-        });
+        mediaRec.initializeAsync().then(function () {
+            div.src = URL.createObjectURL(mediaRec); // Set the div data source. Most important part
+            div.play(); // read the source and display it
+        })
 
-        oMediaCapture = new Capture.MediaCapture();
-        var settings = new Capture.MediaCaptureInitializationSettings();
-        settings.videoDeviceId = deviceInfo.id;
-        settings.streamingCaptureMode = Capture.streamingCaptureMode.audioAndVideo;
-
-        oMediaCapture.initializeAsync(settings).then(function () {
-            isInitialized = true;
-            oDisplayRequest.requestActive();
-            var preview = document.getElementById("windowsPreview");
-            var previewUrl = URL.createObjectURL(oMediaCapture);
-            preview.src = previewUrl;
-            preview.play();
-        });
     }
 };
 
