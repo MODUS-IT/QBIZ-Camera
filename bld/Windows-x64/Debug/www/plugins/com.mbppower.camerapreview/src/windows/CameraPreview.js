59832f1b2772839c3694ec7a10cf5ed6336aa849
lpmusicon <lpmusicon@gmail.com>
2016-05-27 01:48:23 +0200

    WORKINGGG

--- bld/Windows-x64/Debug/www/plugins/com.mbppower.camerapreview/src/windows/CameraPreview.js
+++ bld/Windows-x64/Debug/www/plugins/com.mbppower.camerapreview/src/windows/CameraPreview.js
@@ -1,184 +1,189 @@
-﻿cordova.define("com.mbppower.camerapreview.CameraPreviewWindows10", function(require, exports, module) {
-var cordova = require('cordova'), QBIZCamera = require('./CameraPreview');
+﻿cordova.define("com.mbppower.camerapreview.CameraPreviewWindows10", function (require, exports, module) {
+    var cordova = require('cordova'), QBIZCamera = require('./CameraPreview');
 
-module.exports = {
-    startCamera: function ( ...noParam ) {
-        function styleBody() {
-            return new Promise(function (resolve, reject) {
-                var body = document.body;
-                body.setAttribute('class', '');
-                body.style.background = '#000';
+    module.exports = {
+        startCamera: function (...noParam) {
+            function styleBody() {
+                return new Promise(function (resolve, reject) {
+                    var body = document.body;
+                    body.setAttribute('class', '');
+                    body.style.background = '#000';
 
-                var video = document.createElement("video");
-                video.setAttribute("id", "windowsPreview");
-                video.style.width = "100%";
-                video.style.height = "100%";
-                video.style.display = "block";
-                video.style.position = "fixed";
-                video.style.top = "0";
-                video.style.bottom = "0";
-                video.style.left = "0";
-                video.style.right = "0";
-                body.insertBefore(video, body.firstChild);
-                if (!document.getElementById('windowsPreview')) reject();
-                resolve();
-            });
-        }
-
-        function enterFullscreen() {
-            return new Promise(function (resolve, reject) {
-                var view = Windows.UI.ViewManagement.ApplicationView.getForCurrentView();
-                view.tryEnterFullScreenMode();
-                resolve();
-            });
-        }
-
-        function enumerateCameras() {
-            return new Promise(function (resolve, reject) {
-                var deviceInfo = Windows.Devices.Enumeration.DeviceInformation;
-                deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture)
-                .then(function (devices) {
-                    settings.videoDeviceId = devices[0]; //Use first device
+                    var video = document.createElement("video");
+                    video.setAttribute("id", "windowsPreview");
+                    video.style.width = "100%";
+                    video.style.height = "100%";
+                    video.style.display = "block";
+                    video.style.position = "fixed";
+                    video.style.top = "0";
+                    video.style.bottom = "0";
+                    video.style.left = "0";
+                    video.style.right = "0";
+                    body.insertBefore(video, body.firstChild);
+                    if (!document.getElementById('windowsPreview')) reject();
                     resolve();
-                }, function (err) {
-                    throw new Error('No devices available');
                 });
-            });
-        }
+            }
 
-        function rotateCamera() {
-            var props = mediaCapture.videoDeviceController.getMediaStreamProperties(Windows.Media.Capture.MediaStreamType.videoPreview);
-            props.properties.insert(RotationKey, 90);
-            mediaCapture.setEncodingPropertiesAsync(Windows.Media.Capture.MediaStreamType.videoPreview, props, null);
-        }
+            function enterFullscreen() {
+                return new Promise(function (resolve, reject) {
+                    var view = Windows.UI.ViewManagement.ApplicationView.getForCurrentView();
+                    view.tryEnterFullScreenMode();
+                    resolve();
+                });
+            }
 
-        var mediaCapture = new Windows.Media.Capture.MediaCapture();
-        var settings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
-        var RotationKey = "C380465D-2271-428C-9B83-ECEA3B4A85C1";
+            function enumerateCameras() {
+                return new Promise(function (resolve, reject) {
+                    var deviceInfo = Windows.Devices.Enumeration.DeviceInformation;
+                    deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture)
+                    .then(function (devices) {
+                        settings.videoDeviceId = devices[0]; //Use first device
+                        resolve();
+                    }, function (err) {
+                        throw new Error('No devices available');
+                    });
+                });
+            }
 
-        styleBody()
-        .then(function () {
-            return enterFullscreen();
-        })
-        .then(function () {
-            return enumerateCameras();
-        })
-       .then(function () {
-            return mediaCapture.initializeAsync();
-        })
-       .then(function () {
-            var preview = document.getElementById("windowsPreview");
-            preview.src = URL.createObjectURL(mediaCapture);
-            preview.play();
-            preview.addEventListener("playing", rotateCamera);
-            QBIZCamera.mediaCapture = mediaCapture;
-        });
-    },
-    stopCamera: function( callback, error, noParam ) {
-        return callback(true);
-    },
-    takePicture: function (successCallback, errorCallback, ...noParam) {
+            function rotateCamera() {
+                var props = mediaCapture.videoDeviceController.getMediaStreamProperties(Windows.Media.Capture.MediaStreamType.videoPreview);
+                props.properties.insert(RotationKey, 90);
+                mediaCapture.setEncodingPropertiesAsync(Windows.Media.Capture.MediaStreamType.videoPreview, props, null);
+            }
 
-        var Imaging = Windows.Graphics.Imaging;
-        var format = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg(),
-            imageStream = new Windows.Storage.Streams.InMemoryRandomAccessStream(),
-        //folder = Windows.Storage.ApplicationData.current.localFolder,
-            folder = Windows.Storage.KnownFolders.picturesLibrary;
+            var mediaCapture = new Windows.Media.Capture.MediaCapture();
+            var settings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
+            var RotationKey = "C380465D-2271-428C-9B83-ECEA3B4A85C1";
+
+            styleBody()
+            .then(function () {
+                return enterFullscreen();
+            })
+            .then(function () {
+                return enumerateCameras();
+            })
+           .then(function () {
+               return mediaCapture.initializeAsync();
+           })
+           .then(function () {
+               var preview = document.getElementById("windowsPreview");
+               preview.src = URL.createObjectURL(mediaCapture);
+               preview.play();
+               preview.addEventListener("playing", rotateCamera);
+               QBIZCamera.mediaCapture = mediaCapture;
+           });
+        },
+        stopCamera: function (callback, error, noParam) {
+            return callback(true);
+        },
+        takePicture: function (successCallback, errorCallback, ...noParam) {
+            var Imaging = Windows.Graphics.Imaging;
+            var format = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg(),
+                imageStream = new Windows.Storage.Streams.InMemoryRandomAccessStream(),
+                thumbnailStream = new Windows.Storage.Streams.InMemoryRandomAccessStream(),
+            //folder = Windows.Storage.ApplicationData.current.localFolder,
+                folder = Windows.Storage.KnownFolders.picturesLibrary;
             bitmapDecoder = null,
             bitmapEncoder = null,
             fileName = null,
             outputStream = null;
 
-        function getCurrentDateTime() {
-            var d = new Date();
-            var datetime = d.getFullYear() + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds();
-            return datetime;
-        }
+            function getCurrentDateTime() {
+                var d = new Date();
+                var datetime = "" + d.getFullYear() + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds();
+                return datetime;
+            }
 
-        // Capture to stream
-        return QBIZCamera.mediaCapture.capturePhotoToStreamAsync(format, imageStream)
-        .then(function () {
-            return Imaging.BitmapDecoder.createAsync(imageStream);
-        })
-        .then(function (decoder) {
-            bitmapDecoder = decoder;
-            return Imaging.BitmapEncoder.createForTranscodingAsync(imageStream, bitmapDecoder);
-        })
-        .then(function (encoder) {
-            bitmapEncoder = encoder;
-            bitmapEncoder.bitmapTransform.rotation = Imaging.BitmapRotation.clockwise90Degrees;
-            console.log(encoder);
-            return bitmapEncoder.flushAsync();
-        })
-        .then(function () {
-            fileName = "QBIZCamera" + getCurrentDateTime() + ".jpg";
-            return folder.createFileAsync(fileName, Windows.Storage.CreationCollisionOption.generateUniqueName);
-        })
-        .then(function (file) {
-            fileName = file.name;
-            return file.openAsync(Windows.Storage.FileAccessMode.readWrite);
-        })
-        .then(function (outStream) {
-            outputStream = outStream;
-            return Windows.Storage.Streams.RandomAccessStream.copyAsync(imageStream, outputStream);
-        })
-        .then(function () {
-            outputStream.close();
-            return Imaging.BitmapEncoder.createForTranscodingAsync(imageStream, bitmapDecoder);
-        })
-        .then(function (encoder) {
-            console.log(encoder);
-            bitmapEncoder = encoder;
-            bitmapEncoder.bitmapTransform.scaledWidth = bitmapDecoder.pixelHeight/6;
-            bitmapEncoder.bitmapTransform.scaledHeight = bitmapDecoder.pixelWidth/6;
-            bitmapEncoder.bitmapTransform.interpolationMode = Imaging.BitmapInterpolationMode.cubic;
-            return bitmapEncoder.flushAsync();
-        })
-        .then(function () {
-            return folder.createFileAsync(fileName + ".2.jpg", Windows.Storage.CreationCollisionOption.generateUniqueName);
-        })
-        .then(function (file) {
-            return file.openAsync(Windows.Storage.FileAccessMode.readWrite);
-        })
-        .then(function (outStream) {
-            outputStream = outStream;
-            return Windows.Storage.Streams.RandomAccessStream.copyAsync(imageStream, outputStream);
-        })
-        .then(function (file) {
-            outputStream.close();
-            imageStream.close();
-            window.onPictureTaken(["ms-appdata:///local/" + fileName]);
-        })
-        .done();
-    },
-    setOnPictureTakenHandler: function (onTaken) {
-        window.onPictureTaken = onTaken;
-    },
-    show: function (...noParam) {
-        var preview = document.getElementById('windowsPreview');
-        preview.play();
-    },
-    hide: function (...noParam) {
-        var preview = document.getElementById('windowsPreview');
-        preview.pause();
-    },
-    switchCamera: function(...noParam) {
+            // Capture to stream
+            return QBIZCamera.mediaCapture.capturePhotoToStreamAsync(format, imageStream)
+            .then(function () {
+                return Windows.Storage.Streams.RandomAccessStream.copyAsync(imageStream, thumbnailStream);
+            })
+            .then(function () {
+                return Imaging.BitmapDecoder.createAsync(imageStream);
+            })
+            .then(function (decoder) {
+                bitmapDecoder = decoder;
+                return Imaging.BitmapEncoder.createForTranscodingAsync(imageStream, bitmapDecoder);
+            })
+            .then(function (encoder) {
+                bitmapEncoder = encoder;
+                bitmapEncoder.bitmapTransform.rotation = Imaging.BitmapRotation.clockwise90Degrees;
+                return bitmapEncoder.flushAsync();
+            })
+            .then(function () {
+                fileName = "QBIZCamera" + getCurrentDateTime() + ".jpg";
+                return folder.createFileAsync(fileName, Windows.Storage.CreationCollisionOption.generateUniqueName);
+            })
+            .then(function (file) {
+                fileName = file.name;
+                return file.openAsync(Windows.Storage.FileAccessMode.readWrite);
+            })
+            .then(function (outStream) {
+                outputStream = outStream;
+                return Windows.Storage.Streams.RandomAccessStream.copyAsync(imageStream, outputStream);
+            })
+            .then(function () {
+                outputStream.close();
+                return Imaging.BitmapDecoder.createAsync(thumbnailStream);
+            })
+            .then(function (decoder) {
+                bitmapDecoder = decoder;
+                return Imaging.BitmapEncoder.createForTranscodingAsync(thumbnailStream, bitmapDecoder);
+            })
+            .then(function (encoder) {
+                bitmapEncoder = encoder;
+                bitmapEncoder.bitmapTransform.scaledWidth = bitmapDecoder.pixelHeight / 6;
+                bitmapEncoder.bitmapTransform.scaledHeight = bitmapDecoder.pixelWidth / 6;
+                bitmapEncoder.bitmapTransform.interpolationMode = Imaging.BitmapInterpolationMode.cubic;
+                return bitmapEncoder.flushAsync();
+            })
+            .then(function () {
+                return folder.createFileAsync("QbizThumb.jpg", Windows.Storage.CreationCollisionOption.generateUniqueName);
+            })
+            .then(function (file) {
+                return file.openAsync(Windows.Storage.FileAccessMode.readWrite);
+            })
+            .then(function (outStream) {
+                outputStream = outStream;
+                return Windows.Storage.Streams.RandomAccessStream.copyAsync(thumbnailStream, outputStream);
+            })
+            .then(function (file) {
+                outputStream.close();
+                imageStream.close();
+                window.onPictureTaken(["ms-appdata:///local/" + fileName]);
+            })
+            .done();
+        },
+        setOnPictureTakenHandler: function (onTaken) {
+            window.onPictureTaken = onTaken;
+        },
+        show: function (...noParam) {
+            var preview = document.getElementById('windowsPreview');
+            preview.play();
+        },
+        hide: function (...noParam) {
+            var preview = document.getElementById('windowsPreview');
+            preview.pause();
+        },
+        switchCamera: function (...noParam) {
 
-    },
-    logCamera: function (callback, error, noParam) {
-        //ToDo
-        //Find used resolution
-        //Find preview resolution
-        return callback({
-            resolution: { width: 320, height: 640 },
-            previewResolution: { width: 1337, height: 16 }
-        });
-    },
-    useTimer: function (...noParam) { },
-    useMotionDetection: function (...noParam) { },
-    motionDetectionStart: function (...noParam) { },
-    motionDetectionStop: function(...noParam) {}
-};
+        },
+        logCamera: function (callback, error, noParam) {
+            //ToDo
+            //Find used resolution
+            //Find preview resolution
+            return callback({
+                resolution: { width: 320, height: 640 },
+                previewResolution: { width: 1337, height: 16 }
+            });
+        },
+        useTimer: function (...noParam) { },
+        useMotionDetection: function (...noParam) { },
+        motionDetectionStart: function (...noParam) { },
+        motionDetectionStop: function (...noParam) { }
+    };
 
-require("cordova/exec/proxy").add("CameraPreview", module.exports);
+    require("cordova/exec/proxy").add("CameraPreview", module.exports);
 });
